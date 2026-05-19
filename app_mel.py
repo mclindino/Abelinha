1f2550a169c94aafe5597c537b081d2feba3ad41
mclindino <m203581@dac.unicamp.br>
2026-05-19 19:13:55 -0300

    adicionar Ambas abelhas

--- app_mel.py
+++ app_mel.py
@@ -5,7 +5,24 @@ import os
 st.set_page_config(page_title="Explorador de Mel Jataí", layout="wide")
 
 st.title("🍯 Explorador de Assinaturas Químicas do Mel")
-st.markdown("Selecione os parâmetros abaixo para visualizar como as amostras se agrupam com base na sua composição química (FTIR).")
+
+st.markdown("""
+Este painel transforma os espectros de infravermelho (FTIR) das amostras de mel em visualizações interativas.
+
+### 📉 PCA (Análise de Componentes Principais)
+Resume os dados preservando as maiores variações globais entre as amostras. 
+* **Configuração:** Não há parâmetros para alterar.
+
+### 🌌 UMAP (Aproximação e Projeção de Variedades Uniformes)
+Uma técnica não-linear, focada em agrupar amostras com assinaturas semelhantes (preservando a vizinhança local). O UMAP possue 3 parâmetros principais:
+
+* **`n_neighbors` (Número de Vizinhos):** Valores baixos focam em separar detalhes finos e pequenos grupos. Valores mais altos forçam o algoritmo a focar no panorama geral.
+* **`metric` (Métrica de Distância):** Recomenda-se manter em **Cosseno**. Aavalia o *formato* das bandas, ignorando ruídos de linha de base.
+* **`min_dist` (Distância Mínima):** Controla a dispersão dos pontos. Recomenda-se manter o **0.1**.
+
+---
+**Selecione os parâmetros abaixo para visualizar como as amostras se agrupam com base na sua composição química.**
+""")
 
 opcoes_freq = [
     "Todas", "3800_3015", "3015_2450", 
@@ -15,7 +32,7 @@ opcoes_freq = [
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    abelha_selecionada = st.selectbox("Abelhinha:", ['Jataí', 'Mandaçaia'])
+    abelha_selecionada = st.selectbox("Abelhinha:", ['Jataí', 'Mandaçaia', 'Ambas'])
 
 with col2:
     metodo_selecionado = st.selectbox("Método de Análise:", ['PCA', 'UMAP'])
@@ -35,23 +52,57 @@ if metodo_selecionado == 'UMAP':
         metrica_selecionada = st.selectbox("Métrica de Comparação:", ['cosine', 'euclidean'])
 
     with col_umap2:
-        nn_selecionado = st.selectbox("Tamanho da Vizinhança (N):", [3, 4, 5])
+        if abelha_selecionada == 'Ambas':
+            nn_selecionado = st.selectbox("Tamanho da Vizinhança (N):", [5, 7, 10, 15])
+        else:
+            nn_selecionado = st.selectbox("Tamanho da Vizinhança (N):", [3, 4, 5])
 
     with col_umap3:
         md_selecionado = st.selectbox("Espalhamento (Min Dist):", [0.1, 0.8])
 
-st.divider() 
+st.divider()
 
-if metodo_selecionado == 'PCA':
-    nome_arquivo = f"graficos/{abelha_selecionada}/PCA_{dim}_{freq_selecionada}.html"
-else:
-    nome_arquivo = f"graficos/{abelha_selecionada}/UMAP_{dim}_{freq_selecionada}_{metrica_selecionada}_nn{nn_selecionado}_md{md_selecionado}.html"
-
-if os.path.exists(nome_arquivo):
-    with open(nome_arquivo, 'r', encoding='utf-8') as f:
-        html_data = f.read()
+if abelha_selecionada != 'Ambas':
     
-    components.html(html_data, height=850)
+    if not st.toggle("Resultado PCA/UMAP"):
+        nome_arquivo = f"graficos/{abelha_selecionada}/Frequencias.html"
+        
+        if os.path.exists(nome_arquivo):
+            with open(nome_arquivo, 'r', encoding='utf-8') as f:
+                html_data = f.read()
+            
+            components.html(html_data, height=800)
+        else:
+            st.error(f"⚠️ Gráfico não encontrado! O arquivo procurado foi: {nome_arquivo}")
+            st.info("Verifique se você gerou essa combinação específica ou se a pasta 'graficos' está no lugar certo.")
+        
+        #st.divider()
+    
+    else:
+        if metodo_selecionado == 'PCA':
+            nome_arquivo = f"graficos/{abelha_selecionada}/PCA_{dim}_{freq_selecionada}.html"
+        else:
+            nome_arquivo = f"graficos/{abelha_selecionada}/UMAP_{dim}_{freq_selecionada}_{metrica_selecionada}_nn{nn_selecionado}_md{md_selecionado}.html"
+        
+        if os.path.exists(nome_arquivo):
+            with open(nome_arquivo, 'r', encoding='utf-8') as f:
+                html_data = f.read()
+            
+            components.html(html_data, height=850)
+        else:
+            st.error(f"⚠️ Gráfico não encontrado! O arquivo procurado foi: {nome_arquivo}")
+            st.info("Verifique se você gerou essa combinação específica ou se a pasta 'graficos' está no lugar certo.")
 else:
-    st.error(f"⚠️ Gráfico não encontrado! O arquivo procurado foi: {nome_arquivo}")
-    st.info("Verifique se você gerou essa combinação específica ou se a pasta 'graficos' está no lugar certo.")
+    if metodo_selecionado == 'PCA':
+        nome_arquivo = f"graficos/{abelha_selecionada}/PCA_{dim}_{freq_selecionada}.html"
+    else:
+        nome_arquivo = f"graficos/{abelha_selecionada}/UMAP_{dim}_{freq_selecionada}_{metrica_selecionada}_nn{nn_selecionado}_md{md_selecionado}.html"
+    
+    if os.path.exists(nome_arquivo):
+        with open(nome_arquivo, 'r', encoding='utf-8') as f:
+            html_data = f.read()
+        
+        components.html(html_data, height=850)
+    else:
+        st.error(f"⚠️ Gráfico não encontrado! O arquivo procurado foi: {nome_arquivo}")
+        st.info("Verifique se você gerou essa combinação específica ou se a pasta 'graficos' está no lugar certo.")
